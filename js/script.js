4bebe4525281450e86dca5037671743b3310a45a
michaelc7798 <michaelc7798@gmail.com>
2022-03-08 20:57:35 -0500

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,5 @@
+let pokemonList = [
+  { name: "bobasoar", height: 0.7, type: ["grass", "poison"] },
+  { name: "churmender", height: 0.6, type: "fire" },
+  { name: "skurtle", height: 0.5, type: ["water", "cool"] },
+];
